b8edc148a21348267e30213049e968c674bd16ee
romanfilenko <filenko@uxf.cz>
2020-07-13 18:12:49 +0200

    fix copytext width on laptop+

--- src/components/UrlItem.js
+++ src/components/UrlItem.js
@@ -12,7 +12,7 @@ const UrlItem = ({ result }) => {
     <div className="bg-white border flex flex-wrap items-center justify-between my-4 p-4 rounded text-sm">
       <p className="mb-4 md:mb-0 truncate border-bottom-2">{original}</p>
       <hr className="bg-gray-100 block mb-4 w-full md:hidden border-separate" />
-      <span className="w-full text-teal-400 flex flex-col md:flex-row md:items-center">
+      <span className="w-full md:w-auto text-teal-400 flex flex-col md:flex-row md:items-center">
         {shortened}
 
         <button
